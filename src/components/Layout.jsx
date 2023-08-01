61862cd33dc58079d8d3516cc6005bc8de0917c8
Jacky Lee <dr.jackylee@gmail.com>
2023-08-01 15:18:39 -0700

    Make Navbar fixed

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 const Layout = ({ children, title }) => (
   <div className="px-5 pt-5 pb-60">
     <Navbar />
-    <div className="flex flex-col gap-8 px-6 pt-24 mx-auto max-w-3xl">
+    <div className="flex flex-col gap-8 px-6 pt-40 mx-auto max-w-3xl">
       <h1 className="pb-4 text-xl font-bold tracking-wide text-center sm:pb-8 sm:text-3xl md:pb-16 md:text-5xl font-heading">
         {title}
       </h1>
