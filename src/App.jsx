207802e937b338d8d15d44b77e39dbbd59d65654
Jacky Lee <dr.jackylee@gmail.com>
2023-08-01 09:38:52 -0700

    Make page scroll to top on each link navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import MainContainer from "./components/MainContainer";
+import ScrollToTop from "./components/ScrollToTop";
 
 const App = () => (
   <div className="min-h-screen bg-gradient-to-br from-yellow-300 to-red-300">
     <BrowserRouter>
+      <ScrollToTop />
       <MainContainer />
     </BrowserRouter>
   </div>
