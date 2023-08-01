fc4cd22c2e94e157178ba39067a863f288d445d4
Jacky Lee <dr.jackylee@gmail.com>
2023-08-01 05:24:47 -0700

    Add logo and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['"DM Sans"', "sans-serif"],
+      heading: ["Unbounded", "sans-serif"],
+    },
   },
   plugins: [require("daisyui")],
 };
