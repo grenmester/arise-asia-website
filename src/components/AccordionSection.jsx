af587c32c97849b7e6e355ce1b9444ce755d019a
Jacky Lee <dr.jackylee@gmail.com>
2023-08-01 15:20:19 -0700

    Add bottom border to accordion items

--- src/components/AccordionSection.jsx
+++ src/components/AccordionSection.jsx
@@ -7,7 +7,7 @@ const AccordionSection = ({ items }) => (
           className="bg-orange-300 rounded-none collapse collapse-arrow"
         >
           <input type="checkbox" />
-          <div className="text-lg font-bold sm:text-xl collapse-title">
+          <div className="text-lg font-bold border-b-2 sm:text-xl collapse-title border-b-white">
             {item.title}
           </div>
           <div className="bg-white collapse-content">
